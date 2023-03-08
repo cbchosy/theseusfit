a187db38f3b6ccf9d5bc0cd79a2231684af9b098
cbchosy <100698378+cbchosy@users.noreply.github.com>
2023-03-08 20:14:43 +0000

    Included package data in setup.py

--- setup.py
+++ setup.py
@@ -7,6 +7,8 @@ setuptools.setup(
     description='GPU-accelerated curve fitting using nonlinear optimization from Theseus',
     url='https://github.com/cbchosy/theseusqfls',
     packages=['theseusqfls'],
+    package_dir={'theseusqfls': 'theseusqfls/theseusqfls'},
+    package_data={'theseusqfls': ['data/lookup_table.csv']},
     python_requires='>=3.6',
     install_requires=[
         'theseus',
